1a3a5b816dfbf9410c59095a1f30809ecd30829a
kaiser22081993 <railag22081993@gmail.com>
2016-07-12 10:14:08 +0300

    start making order output

--- src/main/java/oleg/homework/domain/order/Order.java
+++ src/main/java/oleg/homework/domain/order/Order.java
@@ -7,11 +7,9 @@ import java.util.List;
 
 public class Order {
 
-    private int id;
+
     private String customer;
-    private String date;
-    private int estimatedDeliveryTime;
-    private int deliveryTime;
+    private String date = "00.00.00";
     private boolean canceled;
     private String address;
     private String phone;
@@ -19,24 +17,12 @@ public class Order {
     private List<DeliveryItem> item;
 
     public Order(){
-
-    }
-    public Order(String customer, String phone, String address, int deliveryTime, int estimatedDeliveryTime, String date) {
-        this.customer = customer;
-        this.phone = phone;
-        this.address = address;
-        this.deliveryTime = deliveryTime;
-        this.estimatedDeliveryTime = estimatedDeliveryTime;
-        this.date = date;
-        canceled = false;
     }
 
     public void updateFrom(Order o){
         o.setAddress(o.getAddress());
         o.setCustomer(o.getCustomer());
         o.setDate(o.getDate());
-        o.setDeliveryTime(o.getDeliveryTime());
-        o.setEstimatedDeliveryTime(o.getEstimatedDeliveryTime());
         o.setPhone(o.getPhone());
     }
 
@@ -56,14 +42,6 @@ public class Order {
         this.item = item;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getCustomer() {
         return customer;
     }
@@ -80,21 +58,7 @@ public class Order {
         this.date = date;
     }
 
-    public int getEstimatedDeliveryTime() {
-        return estimatedDeliveryTime;
-    }
 
-    public void setEstimatedDeliveryTime(int estimatedDeliveryTime) {
-        this.estimatedDeliveryTime = estimatedDeliveryTime;
-    }
-
-    public int getDeliveryTime() {
-        return deliveryTime;
-    }
-
-    public void setDeliveryTime(int deliveryTime) {
-        this.deliveryTime = deliveryTime;
-    }
 
     public String getAddress() {
         return address;
--- src/main/webapp/resources/modules/content/content.js
+++ src/main/webapp/resources/modules/content/content.js
@@ -1,127 +1,163 @@
-
-
 var nextUrl = "rest/pizza";
 var prevUrl = "rest/pizza";
 angular.module('Content')
-.controller('myCtrl',function ($scope, $http, $rootScope ) {
-    console.log("next ----");
-    $http({
-        method : "GET",
-        url : nextUrl
-
-    }).then(function mySucces(response) {
-        $scope.pizzas = response.data;
-        $scope.hs = response.headers("link");
-        console.log(response.headers());
-        $rootScope.pizzas = response.data;
-
-
-        nextUrl = parseLinkHeader(response.headers("link"))["next"]["href"];
-        prevUrl = parseLinkHeader(response.headers("link"))["previous"]["href"];
-
-    }, function myError(response) {
-        $scope.myWelcome = response.statusText;
-    });
-
-    $rootScope.next = function(){
-        console.log("next func!");
-        $(".cells_cont").hide(200);
-
+    .controller('myCtrl', function ($scope, $http, $rootScope) {
+        console.log("next ----");
         $http({
-            method : "GET",
-            url : nextUrl
+            method: "GET",
+            url: nextUrl
 
         }).then(function mySucces(response) {
-            console.log("next func then");
             $scope.pizzas = response.data;
             $scope.hs = response.headers("link");
             console.log(response.headers());
+            $rootScope.pizzas = response.data;
+
+
             nextUrl = parseLinkHeader(response.headers("link"))["next"]["href"];
             prevUrl = parseLinkHeader(response.headers("link"))["previous"]["href"];
-
 
         }, function myError(response) {
             $scope.myWelcome = response.statusText;
         });
-        $(".cells_cont").show(200);
-    };
-    $rootScope.previous = function(){
-        console.log("next func!");
-        $(".cells_cont").hide(200);
 
-        $http({
-            method : "GET",
-            url : prevUrl
+        $rootScope.next = function () {
+            console.log("next func!");
+            $(".cells_cont").hide(200);
 
-        }).then(function mySucces(response) {
-            console.log("next func then");
-            $scope.pizzas = response.data;
-            $scope.hs = response.headers("link");
-            console.log(response.headers());
-            nextUrl = parseLinkHeader(response.headers("link"))["next"]["href"];
-            prevUrl = parseLinkHeader(response.headers("link"))["previous"]["href"];
+            $http({
+                method: "GET",
+                url: nextUrl
+
+            }).then(function mySucces(response) {
+                console.log("next func then");
+                $scope.pizzas = response.data;
+                $scope.hs = response.headers("link");
+                console.log(response.headers());
+                nextUrl = parseLinkHeader(response.headers("link"))["next"]["href"];
+                prevUrl = parseLinkHeader(response.headers("link"))["previous"]["href"];
 
 
-        }, function myError(response) {
-            $scope.myWelcome = response.statusText;
-        });
-        $(".cells_cont").show(200);
-    };
-    $scope.firstName = "Oleh";
-    $scope.lastName = "Kapustkin";
-
-    $rootScope.ordered = [];
-    $rootScope.totalSum = 0;
-
-    $scope.getOne = function(index){
-        $scope.targetPizza = $scope.pizzas[index];
-    };
-    $scope.isInOrders = function(title) {
-        for(var i = 0; i < $rootScope.ordered.length; i++){
-            if($rootScope.ordered[i].pizza.title === title){
-                return true;
-            }
-        }
-        return false;
-    };
-    $scope.findInOrders = function(title){
-        for(var i = 0; i < $rootScope.ordered.length; i++){
-            if($rootScope.ordered[i].pizza.title === title){
-                return $rootScope.ordered[i];
-            }
-        }
-        return null;
-    };
-    $scope.addToOrders = function addT(index){
-        var item = {
-            pizza:$scope.pizzas[index],
-            quantity:1
+            }, function myError(response) {
+                $scope.myWelcome = response.statusText;
+            });
+            $(".cells_cont").show(200);
         };
-        if( !$scope.isInOrders($scope.pizzas[index].title)){
-            $rootScope.ordered.push(item);
-            $rootScope.totalSum += item.pizza.price;
-        }
-        else {
-            $scope.findInOrders(item.pizza.title).quantity++;
-            $rootScope.totalSum += item.pizza.price;
-        }
+        $rootScope.previous = function () {
+            console.log("next func!");
+            $(".cells_cont").hide(200);
 
-    };
-        $rootScope.changeOrderQuantity = function(index) {
+            $http({
+                method: "GET",
+                url: prevUrl
+
+            }).then(function mySucces(response) {
+                console.log("next func then");
+                $scope.pizzas = response.data;
+                $scope.hs = response.headers("link");
+                console.log(response.headers());
+                nextUrl = parseLinkHeader(response.headers("link"))["next"]["href"];
+                prevUrl = parseLinkHeader(response.headers("link"))["previous"]["href"];
+
+
+            }, function myError(response) {
+                $scope.myWelcome = response.statusText;
+            });
+            $(".cells_cont").show(200);
+        };
+        $scope.firstName = "Oleh";
+        $scope.lastName = "Kapustkin";
+
+        $rootScope.ordered = [];
+        $rootScope.totalSum = 0;
+
+        $scope.getOne = function (index) {
+            $scope.targetPizza = $scope.pizzas[index];
+        };
+        $scope.isInOrders = function (title) {
+            for (var i = 0; i < $rootScope.ordered.length; i++) {
+                if ($rootScope.ordered[i].pizza.title === title) {
+                    return true;
+                }
+            }
+            return false;
+        };
+        $scope.findInOrders = function (title) {
+            for (var i = 0; i < $rootScope.ordered.length; i++) {
+                if ($rootScope.ordered[i].pizza.title === title) {
+                    return $rootScope.ordered[i];
+                }
+            }
+            return null;
+        };
+        $scope.addToOrders = function addT(index) {
+            var item = {
+                pizza: $scope.pizzas[index],
+                quantity: 1
+            };
+            if (!$scope.isInOrders($scope.pizzas[index].title)) {
+                $rootScope.ordered.push(item);
+                $rootScope.totalSum += item.pizza.price;
+            }
+            else {
+                $scope.findInOrders(item.pizza.title).quantity++;
+                $rootScope.totalSum += item.pizza.price;
+            }
+
+        };
+        $rootScope.changeOrderQuantity = function (index) {
 
             var sum = 0;
-            $rootScope.ordered.forEach(function(p){
-                sum += p.pizza.price * p.quantity;
-            });
+            try{
+                $rootScope.ordered.forEach(function (p) {
+                    sum += p.pizza.price * p.quantity;
+                });
+            }
+            catch (e){
+                alert('wrong input');
+            }
             $rootScope.totalSum = sum;
-        }
+        };
+        $rootScope.fields = {
+            userName : "",
+            userAddress : "",
+            userPhone : ""
+        };
+
+        $rootScope.orders = [];
+        $rootScope.commitOrder = function () {
+
+            var order = {
+                customer: $scope.fields.userName,
+                date: "today",
+                canceled: false,
+                address: $scope.fields.userAddress,
+                phone: $scope.fields.userPhone,
+
+                item : $rootScope.ordered
+            };
+            $rootScope.orders.push(order);
+            if($scope.fields.userName === "" || $scope.fields.userAddress === "" || $scope.fields.userPhone === ""){
+                alert('All fields must be completed!');
+                return;
+            }
+            $http({
+                method: "POST",
+                url: "rest/order",
+                data: order
+
+            }).then(function mySucces(response) {
+                alert('order added!')
+            }, function myError(response) {
+                alert(response.statusText);
+            });
+
+            $rootScope.totalSum = 0;
+            $rootScope.ordered = [];
+        };
 
 
-});
-
-
-
-
+    });
 
 
 // Unquote string (utility)
@@ -160,18 +196,16 @@ function parseLinkHeader(header) {
 }
 
 
-
-
-function format(s){
-    var newS="";
+function format(s) {
+    var newS = "";
     var flag = false;
-    for (var i = 0;i < s.length; i++) {
-        if(s[i]==="/"){
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] === "/") {
             flag = true;
             continue;
         }
-        if(s[i]===">")return "rest/" + newS;
-        if(flag){
+        if (s[i] === ">")return "rest/" + newS;
+        if (flag) {
             newS += s[i];
         }
 
